41b5dd902a0947799ebe3c925384994a3c83dc1e
fbaudet <42float@gmail.com>
2015-04-09 21:04:55 +0200

    add xpm border files

--- customize.sh
+++ customize.sh
@@ -1,1 +1,3 @@
-
+#!/bin/bash
+sudo cp -t /usr/share/themes/Greybird/xfwm4 src/bottom-active.xpm src/bottom-left-active.xpm src/bottom-right-active.xpm src/left-active.xpm src/right-active.xpm
+xfwm4 --replace
